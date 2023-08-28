7490314497467203e079c7e268917ad5943bad54
Mehedi0101 <mehedih2909@gmail.com>
2023-08-28 20:36:49 +0600

    modal added for all the phones

--- scripts/phone.js
+++ scripts/phone.js
@@ -27,8 +27,8 @@ const displayPhones = (list,isShowAll) => {
         <div class="card-body">
             <h2 class="card-title">${item.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="card-actions justify-center">
+                <button class="btn btn-primary" onclick="showDetails('${item.slug}'); details.showModal()">SHOW DETAILS</button>
             </div>
         </div>
         `
@@ -55,3 +55,30 @@ const spinnerToggle = (loadStatus) => {
 document.getElementById('show-button').addEventListener('click',()=>{
     displayItems(true);
 })
+
+const showDetails = async (id) => { 
+        const modalContainer = document.getElementById('modal-container');
+        modalContainer.textContent='';
+        const response = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+        const data = await response.json();
+        const phoneDetails = data.data;
+        console.log(phoneDetails);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${phoneDetails.image}" alt="" class="mx-auto">
+        <h2 class="text-2xl font-semibold">${phoneDetails.name}</h2>
+        <p><span class="font-bold">Storage: </span>${phoneDetails.mainFeatures.storage}</p>
+        <p><span class="font-bold">Display Size: </span>${phoneDetails.mainFeatures.displaySize}</p>
+        <p><span class="font-bold">Chipset: </span>${phoneDetails.mainFeatures.chipset}</p>
+        <p><span class="font-bold">Memory: </span>${phoneDetails.mainFeatures.memory}</p>
+        <p><span class="font-bold">Release data: </span>${phoneDetails.releaseDate}</p>
+        <p><span class="font-bold">Brand: </span>${phoneDetails.brand}</p>
+        <p><span class="font-bold">GPS: </span>${phoneDetails?.others?.GPS || 'none'}</p>
+        <div class="modal-action">
+            <button class="btn">Close</button>
+        </div>
+        `
+        modalContainer.appendChild(div);
+}
+
+
